03d51a104716404155250fc1442b611c8040d0d4
hhjjpp <hyejung8053@gmail.com>
2020-09-25 20:58:57 +0900

    lab4

--- src/containers/TodoList/RealDetail/RealDetail.js
+++ src/containers/TodoList/RealDetail/RealDetail.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+import * as actionTypes from '../../../store/actions/actionTypes';
+import * as actionCreators from '../../../store/actions/index';
+
 import './RealDetail.css';
 
 class RealDetail extends Component {
+  componentDidMount() {
+    this.props.onGetTodo(this.props.match.params.id);
+  }
+
   render() {
+    let title = '';
+    let content = '';
+    if (this.props.selectedTodo) {
+      title = this.props.selectedTodo.title;
+      content = this.props.selectedTodo.content;
+    }
+
     return (
       <div className="RealDetail" >
         <div className="row">
           <div className="left">
-            Name:
+            Name:{title}
         </div>
           <div className="right">
           </div>
         </div>
         <div className="row">
           <div className="left">
-            Content:
+            Content:{content}
         </div>
           <div className="right">
           </div>
@@ -25,4 +40,16 @@ class RealDetail extends Component {
   }
 };
 
-export default RealDetail;
+const mapstateToProps = dispatch => {
+  return {
+    onGetTodo: id => dispatch(actionCreators.getTodo(id)),
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onGetTodo: id => dispatch(actionCreators.getTodo(id))
+  }
+}
+
+export default connect(mapstateToProps, mapDispatchToProps)(RealDetail);
