5211c1c32cbba5a251e2323a8ce9a47857a6b454
kichang <kichang>
2020-09-25 15:52:24 +0900

    redux tutorial complete

--- src/containers/TodoList/RealDetail/RealDetail.js
+++ src/containers/TodoList/RealDetail/RealDetail.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import * as actionTypes from '../../../store/actions/actionTypes';
+import * as actionCreators from '../../../store/actions/index';
 import './RealDetail.css';
 
 class RealDetail extends Component {
+
+  componentDidMount() {
+    // it’s safe to get state after component mounted 
+    this.props.onGetTodo(parseInt(this.props.match.params.id));
+  }
+
+
   render() {
+    let title = ''; let content = ''; 
+    if (this.props.selectedTodo) {
+      title = this.props.selectedTodo.title;
+      content = this.props.selectedTodo.content; 
+    }
+    
     return (
       <div className="RealDetail" >
         <div className="row">
           <div className="left">
             Name:
-        </div>
+          </div>
           <div className="right">
+            {title}
           </div>
         </div>
         <div className="row">
           <div className="left">
             Content:
-        </div>
+          </div>
           <div className="right">
+            {content}
           </div>
         </div>
       </div>
@@ -25,4 +42,15 @@ class RealDetail extends Component {
   }
 };
 
-export default RealDetail;
+
+const mapStateToProps = state => {
+  return {
+    selectedTodo: state.td.selectedTodo, };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    onGetTodo: id => dispatch(actionCreators.getTodo(id)),
+  }; 
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RealDetail);
