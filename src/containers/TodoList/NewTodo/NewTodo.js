a2573a001ef560e62dc4170a53e5cd7e9b8b99ea
wafflejuice <wafflejuice96@gmail.com>
2020-09-25 17:57:00 +0900

    work

--- src/containers/TodoList/NewTodo/NewTodo.js
+++ src/containers/TodoList/NewTodo/NewTodo.js
@@ -4,27 +4,23 @@ import { Redirect } from 'react-router-dom';
 
 import './NewTodo.css';
 
+import {connect} from 'react-redux'
+import * as actionTypes from "../../../store/actions/actionTypes";
+
+import * as actionCreators from '../../../store/actions/index';
+
 class NewTodo extends Component {
   state = {
     title: '',
     content: '',
-    submitted: false
+    //submitted: false
   }
 
   postTodoHandler = () => {
-    const data =
-      { title: this.state.title, content: this.state.content }
-    alert('submitted' + data.title);
-    // this.props.history.push('/todos');
-    this.props.history.goBack();
-    this.setState({ submitted: true });
+   this.props.onStoreTodo(this.state.title, this.state.content);
   }
 
   render() {
-    let redirect = null;
-    if (this.state.submitted) {
-      redirect = <Redirect to='/todos' />
-    }
     return (
       <div className="NewTodo">
         <h1>Add a New Todo!</h1>
@@ -45,4 +41,11 @@ class NewTodo extends Component {
   }
 }
 
-export default NewTodo;
+const mapDispatchToProps = dispatch => {
+  return {
+    onStoreTodo: (title, content) =>
+    dispatch(actionCreators.postTodo({title: title, content: content})),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(NewTodo);
