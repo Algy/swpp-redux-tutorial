4eab73a545b29c99e8615ab681803123d6fb4dae
taekop <taekop@naver.com>
2020-09-25 17:59:01 +0900

    completed

--- src/containers/TodoList/NewTodo/NewTodo.js
+++ src/containers/TodoList/NewTodo/NewTodo.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 import { Redirect } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+import * as actionTypes from '../../../store/actions/actionTypes';
+import * as actionCreators from '../../../store/actions/index';
+
 import './NewTodo.css';
 
 class NewTodo extends Component {
@@ -12,12 +16,10 @@ class NewTodo extends Component {
   }
 
   postTodoHandler = () => {
-    const data =
-      { title: this.state.title, content: this.state.content }
-    alert('submitted' + data.title);
-    // this.props.history.push('/todos');
+    this.props.onStoreTodo(
+    this.state.title,
+    this.state.content);
     this.props.history.goBack();
-    this.setState({ submitted: true });
   }
 
   render() {
@@ -36,7 +38,7 @@ class NewTodo extends Component {
         ></input>
         <label>Content</label>
         <textarea rows="4" type="text" value={this.state.content}
-          onChange={(event) => this.setState({ content: event.target.content })}
+          onChange={(event) => this.setState({ content: event.target.value })}
         >
         </textarea>
         <button onClick={() => this.postTodoHandler()}>Submit</button>
@@ -45,4 +47,12 @@ class NewTodo extends Component {
   }
 }
 
-export default NewTodo;
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onStoreTodo: (title, content) =>
+    dispatch(actionCreators.postTodo({title: title, content:  content})),
+    };
+  };
+
+export default connect(null, mapDispatchToProps)(NewTodo);
